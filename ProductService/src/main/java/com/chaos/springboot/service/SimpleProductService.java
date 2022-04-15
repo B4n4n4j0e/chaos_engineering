6a48ce55f792984db0116036ba0fb11d29563f6a
b4n4n4j0e <marcel.solerlopez@stud.uni-bamberg.de>
2022-04-15 20:17:41 +0200

    added correct exception handling

--- ProductService/src/main/java/com/chaos/springboot/service/SimpleProductService.java
+++ ProductService/src/main/java/com/chaos/springboot/service/SimpleProductService.java
@@ -7,7 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.ConcurrentSkipListMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
@@ -22,6 +24,7 @@ public class SimpleProductService implements ProductService {
     /**
      * Returns current list of Products
      * and the command line arguments.
+     *
      * @param page An int containing the requested page
      * @return list of ProductDtos .
      */
@@ -40,6 +43,7 @@ public class SimpleProductService implements ProductService {
 
     /**
      * Returns ProductDto by Id
+     *
      * @param id An Long containing the ean of the Product
      * @return ProductDto .
      * @throws ResponseStatusException if entitiy not found
@@ -56,14 +60,21 @@ public class SimpleProductService implements ProductService {
 
     /**
      * Creates new ProductDto and adds it to the local list
+     *
      * @param newProduct ProductDTO to be created
      * @return new ProductDto
-     * @throws ResponseStatusException if entity already exists
+     * @throws ResponseStatusException if entity already exists or wrong ean type
      */
     public ProductDto createProduct(ProductDto newProduct) {
-        if (products.containsKey(Long.parseLong(newProduct.getEan()))) {
+        try {
+            if (products.containsKey(Long.parseLong(newProduct.getEan()))) {
+                throw new ResponseStatusException(
+                        HttpStatus.BAD_REQUEST, "entity already exists"
+                );
+            }
+        } catch (NumberFormatException e) {
             throw new ResponseStatusException(
-                    HttpStatus.BAD_REQUEST, "entity already exists"
+                    HttpStatus.BAD_REQUEST, "EAN must be a number"
             );
         }
         Product product = converter.convertToEntity(newProduct);
@@ -71,27 +82,36 @@ public class SimpleProductService implements ProductService {
         newProduct.setRating(0.0);
         newProduct.setRatingCounter(0);
         return newProduct;
+
     }
 
     /**
      * Updates ProductDto in local list
+     *
      * @param newProduct A ProductDTO
      * @return new ProductDto
-     * @throws ResponseStatusException if entitiy not found
+     * @throws ResponseStatusException if entitiy not found or wrong ean type
      */
     public ProductDto updateProduct(ProductDto newProduct) {
-        if (products.containsKey(Long.parseLong(newProduct.getEan()))) {
-            Product product = products.get(Long.parseLong(newProduct.getEan()));
-            product.setName(newProduct.getName());
-            return newProduct;
+        try {
+            if (products.containsKey(Long.parseLong(newProduct.getEan()))) {
+                Product product = products.get(Long.parseLong(newProduct.getEan()));
+                product.setName(newProduct.getName());
+                return newProduct;
+            }
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "entity not found"
+            );
+        } catch (NumberFormatException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "EAN must be a number"
+            );
         }
-        throw new ResponseStatusException(
-                HttpStatus.NOT_FOUND, "entity not found"
-        );
     }
 
     /**
      * Deletes ProductDto in local list
+     *
      * @param id of ProductDTO to be deleted
      * @throws ResponseStatusException if entitiy not found
      */
@@ -106,26 +126,34 @@ public class SimpleProductService implements ProductService {
 
     /**
      * Updates Rating for ProductDto in local list
+     *
      * @param ratedProduct ProductDTO to be evaluated
      * @return updated ProductDTO
-     * @throws ResponseStatusException if entitiy not found
+     * @throws ResponseStatusException if entitiy not found or wrong ean type
      */
     @Override
     public ProductDto submitRating(ProductDto ratedProduct) {
-        if (products.containsKey(Long.parseLong(ratedProduct.getEan()))) {
-            Product product = products.get(Long.parseLong(ratedProduct.getEan()));
-            product.incrementRatingCounter();
-            double newValue = product.getRating() + ratedProduct.getRating();
-            product.setRating(newValue);
-            return  converter.convertToDto(product);
+        try {
+            if (products.containsKey(Long.parseLong(ratedProduct.getEan()))) {
+                Product product = products.get(Long.parseLong(ratedProduct.getEan()));
+                product.incrementRatingCounter();
+                double newValue = product.getRating() + ratedProduct.getRating();
+                product.setRating(newValue);
+                return converter.convertToDto(product);
+            }
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "entity not found"
+            );
+        } catch (NumberFormatException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "EAN must be a number"
+            );
         }
-        throw new ResponseStatusException(
-                HttpStatus.NOT_FOUND, "entity not found"
-        );
     }
 
     /**
      * Returns Top K ProductDto list by rating
+     *
      * @param k size of list
      * @return list of top k ProductDtos
      */
